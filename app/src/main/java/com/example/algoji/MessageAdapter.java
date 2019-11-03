57966679daa5c816357e98bcc520d60d66d21bb5
mr-darksoul <akonvs.manav@gmail.com>
2019-11-03 18:05:58 +0530

    Chat Screen

---
+++ app/src/main/java/com/example/algoji/MessageAdapter.java
@@ -0,0 +1,49 @@
+package com.example.algoji;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class MessageAdapter extends ArrayAdapter<UserMessage> {
+    public MessageAdapter(Context context, int resource, List<UserMessage> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_message, parent, false);
+        }
+
+        ImageView photoImageView =  convertView.findViewById(R.id.photoImageView);
+        TextView messageTextView = convertView.findViewById(R.id.messageTextView);
+        TextView authorTextView =  convertView.findViewById(R.id.nameTextView);
+
+        UserMessage message = getItem(position);
+
+        boolean isPhoto = message.getPhotoUrl() != null;
+        if (isPhoto) {
+            messageTextView.setVisibility(View.GONE);
+            photoImageView.setVisibility(View.VISIBLE);
+            Glide.with(photoImageView.getContext())
+                    .load(message.getPhotoUrl())
+                    .into(photoImageView);
+        } else {
+            messageTextView.setVisibility(View.VISIBLE);
+            photoImageView.setVisibility(View.GONE);
+            messageTextView.setText(message.getText());
+        }
+        authorTextView.setText(message.getName());
+
+        return convertView;
+    }
+}
